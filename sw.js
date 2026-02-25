ce459fb8ba36a3868927e3186d8fef9177971c3b
Anshu2maan <anshumaannathchoudhary@gmail.com>
2026-02-26 01:01:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 // sw.js
 const CACHE_NAME = 'mempass-v2.3';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/constants.js',
-    '/crypto.js',
-    '/utils.js',
-    '/password-generator.js',
-    '/vault.js',
-    '/document-vault.js',
-    '/ui-handlers.js',
-    '/main.js',
+    '/mempass/',
+    '/mempass/index.html',
+    '/mempass/style.css',
+    '/mempass/constants.js',
+    '/mempass/crypto.js',
+    '/mempass/utils.js',
+    '/mempass/password-generator.js',
+    '/mempass/vault.js',
+    '/mempass/document-vault.js',
+    '/mempass/ui-handlers.js',
+    '/mempass/main.js',
     'https://cdnjs.cloudflare.com/ajax/libs/argon2-browser/1.18.0/argon2-bundled.min.js',
     'https://unpkg.com/dexie@3.2.3/dist/dexie.js'
 ];
